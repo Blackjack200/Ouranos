68778c1e2fa5df7ed5bd35b807e6375e4be7734f
Blackjack200 <lx826444126@gmail.com>
2025-02-13 18:55:08 +0800

    remove BlockState upgrader

--- src/main/java/com/github/blackjack200/ouranos/data/bedrock/block/BlockStateDeserializeException.java
+++
@@ -1,7 +0,0 @@
-package com.github.blackjack200.ouranos.data.bedrock.block;
-
-public class BlockStateDeserializeException extends RuntimeException{
-    public BlockStateDeserializeException(String message) {
-        super(message);
-    }
-}
