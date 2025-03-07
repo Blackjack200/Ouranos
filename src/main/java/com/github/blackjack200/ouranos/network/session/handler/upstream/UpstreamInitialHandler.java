cc96c3a2fb2b56d88be170ed508378a69f4325d8
Blackjack200 <lx826444126@gmail.com>
2025-03-07 23:49:44 +0800

    ...

--- src/main/java/com/github/blackjack200/ouranos/network/session/handler/upstream/UpstreamInitialHandler.java
+++ src/main/java/com/github/blackjack200/ouranos/network/session/handler/upstream/UpstreamInitialHandler.java
@@ -115,11 +115,7 @@ public class UpstreamInitialHandler implements BedrockPacketHandler {
                     if (Ouranos.getOuranos().getConfig().debug && !(pk instanceof PlayerAuthInputPacket)) {
                         log.debug("C->S {}", pk.getClass());
                     }
-                    if (!(pk instanceof SetEntityMotionPacket)) {
-                        session.upstream.sendPacket(pk);
-                    } else {
-                        session.upstream.sendPacketImmediately(packet);
-                    }
+                    session.upstream.sendPacket(pk);
                 }
             }
         });
@@ -131,11 +127,7 @@ public class UpstreamInitialHandler implements BedrockPacketHandler {
                     if (Ouranos.getOuranos().getConfig().debug && !(pk instanceof PlayerAuthInputPacket) && !(pk instanceof LevelChunkPacket) && !(pk instanceof NetworkChunkPublisherUpdatePacket)) {
                         log.debug("S->C {}", pk);
                     }
-                    if (!(pk instanceof SetEntityMotionPacket)) {
-                        session.downstream.sendPacket(pk);
-                    } else {
-                        session.downstream.sendPacketImmediately(packet);
-                    }
+                    session.downstream.sendPacket(pk);
                 }
             }
         });
